92c9dd5c93b00ac5a421fe49fdd7bc19fa5a27e6
why <asymptoticallystable@gmail.com>
2015-10-06 10:50:08 -0400

    initial commit for push to gitHub pwd

---
+++ MyMathUtilities/MyMathUtilities.m
@@ -0,0 +1,16 @@
+(* Mathematica Package *)
+
+(* Created by the Wolfram Workbench Sep 13, 2015 *)
+
+BeginPackage["MyMathUtilities`", {"JLink`"}]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
